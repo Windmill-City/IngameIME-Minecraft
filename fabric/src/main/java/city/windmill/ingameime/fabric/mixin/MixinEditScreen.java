7df88f74bae996d2242febebc1a94c6b13021094
Windmill-City <1449182174@qq.com>
2021-01-27 20:50:10 +0800

    try support 1.15 foge

--- fabric/src/main/java/city/windmill/ingameime/fabric/mixin/MixinEditScreen.java
+++ fabric/src/main/java/city/windmill/ingameime/fabric/mixin/MixinEditScreen.java
@@ -1,11 +1,15 @@
 package city.windmill.ingameime.fabric.mixin;
 
 import city.windmill.ingameime.fabric.ScreenEvents;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
 import kotlin.Pair;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.BookEditScreen;
 import net.minecraft.client.gui.screens.inventory.SignEditScreen;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -44,20 +48,14 @@ abstract class MixinSignEditScreen extends Screen {
             at = {
                     @At(value = "INVOKE",
                             target = "Lnet/minecraft/client/gui/Font;drawInBatch(Ljava/lang/String;FFIZLcom/mojang/math/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;ZII)I",
-                            ordinal = 1)
-//                    @At(value = "INVOKE",
-//                            target = "Lnet/minecraft/client/gui/GuiComponent;fill(Lcom/mojang/math/Matrix4f;IIIII)V",
-//                            ordinal = 0)
+                            ordinal = 1),
+                    @At(value = "INVOKE",
+                            target = "net/minecraft/client/gui/screens/inventory/SignEditScreen.fill(Lcom/mojang/math/Matrix4f;IIIII)V",
+                            ordinal = 0)
             },
-            locals = LocalCapture.PRINT)
-    private void onCaret_Sign(CallbackInfo ci) {
-        //u(25)->x,q(20)->y
-        //ScreenEvents.INSTANCE.getEDIT_CARET().invoker().onEditCaret(this, new Pair<>((int) matrix4f.m03 + u, (int) matrix4f.m13 + q));
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onCaret_Sign(int i, int j, float f, CallbackInfo ci, PoseStack poseStack, float g, BlockState blockState, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource bufferSource, float k, int l, String[] strings, Matrix4f matrix4f, int n, int o, int p, int q, int v, String string2, int w, int x) {
+        //x(25/24)->x,q(20)->y
+        ScreenEvents.INSTANCE.getEDIT_CARET().invoker().onEditCaret(this, new Pair<>((int) matrix4f.m03 + x, (int) matrix4f.m13 + q));
     }
-
-//    @Surrogate
-//    private void onCaret_Sign(int i, int j, float f, CallbackInfo ci, PoseStack poseStack, float g, BlockState blockState, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource bufferSource, float k, int l, String strings[], Matrix4f matrix4f, int n, int o, int p, int q, int r, String string, int t, int u) {
-//        //u(25)->x,q(20)->y
-//        ScreenEvents.INSTANCE.getEDIT_CARET().invoker().onEditCaret(this, new Pair<>((int) matrix4f.m03 + u, (int) matrix4f.m13 + q));
-//    }
 }
--- forge/src/main/java/city/windmill/ingameime/forge/mixin/MixinEditScreen.java
+++ forge/src/main/java/city/windmill/ingameime/forge/mixin/MixinEditScreen.java
@@ -2,6 +2,7 @@ package city.windmill.ingameime.forge.mixin;
 
 import city.windmill.ingameime.forge.IngameIMEClient;
 import city.windmill.ingameime.forge.ScreenEvents;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
 import kotlin.Pair;
 import net.minecraft.client.gui.screens.Screen;
@@ -51,17 +52,12 @@ abstract class MixinEditSignScreen extends Screen {
                             target = "Lnet/minecraft/client/gui/Font;drawInBatch(Ljava/lang/String;FFIZLcom/mojang/math/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;ZII)I",
                             ordinal = 1),
                     @At(value = "INVOKE",
-                            target = "Lnet/minecraft/client/gui/GuiComponent;fill(Lcom/mojang/math/Matrix4f;IIIII)V",
-                            ordinal = 0)},
-            locals = LocalCapture.PRINT)
-    private void onCaret_Sign(int i, int j, float f, CallbackInfo ci, float g, BlockState lv, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource lv2, float k, int l, int m, int n, int o, Matrix4f lv5, int p, String string, float q, int r, int s) {
-        //s(23)->x,o(17)->y
-        //IngameIMEClient.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((int) lv5.m03 + s, (int) lv5.m13 + o)));
+                            target = "net/minecraft/client/gui/screens/inventory/SignEditScreen.fill(Lcom/mojang/math/Matrix4f;IIIII)V",
+                            ordinal = 0)
+            },
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onCaret_Sign(int i, int j, float f, CallbackInfo ci, PoseStack poseStack, float g, BlockState blockState, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource bufferSource, float k, int l, String[] strings, Matrix4f matrix4f, int n, int o, int p, int q, int v, String string2, int w, int x) {
+        //x(25/24)->x,q(20)->y
+        IngameIMEClient.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((int) matrix4f.m03 + x, (int) matrix4f.m13 + q)));
     }
-
-//    @Surrogate
-//    private void onCaret_Sign(int i, int j, float f, CallbackInfo ci, PoseStack poseStack, float g, BlockState blockState, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource bufferSource, float k, int l, String strings[], Matrix4f matrix4f, int n, int o, int p, int q, int r, String string, int t, int u) {
-//        //u(25)->x,q(20)->y
-//        IngameIMEClient.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((int) matrix4f.m03 + u, (int) matrix4f.m13 + q)));
-//    }
 }
