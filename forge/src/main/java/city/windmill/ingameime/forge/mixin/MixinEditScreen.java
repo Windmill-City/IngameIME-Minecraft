4b2a45f84c7757c0eb61df8b5732c27147331b61
Windmill-City <1449182174@qq.com>
2021-01-28 20:16:58 +0800

    remove debug

--- forge/src/main/java/city/windmill/ingameime/forge/mixin/MixinEditScreen.java
+++ forge/src/main/java/city/windmill/ingameime/forge/mixin/MixinEditScreen.java
@@ -11,7 +11,6 @@ import net.minecraft.client.gui.screens.inventory.SignEditScreen;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -48,7 +47,6 @@ abstract class MixinEditSignScreen extends Screen {
         super(p_i51108_1_);
     }
 
-    @Debug(print = true, export = true)
     @Inject(method = "render",
             at = {
                     @At(value = "INVOKE",
