78ac575989161774c76b75c4e6cb576491ecdb77
Windmill-City <1449182174@qq.com>
2021-02-08 23:03:26 +0800

    add debug log

--- common/src/main/kotlin/city/windmill/ingameime/client/jni/NativeLoader.kt
+++ common/src/main/kotlin/city/windmill/ingameime/client/jni/NativeLoader.kt
@@ -12,9 +12,9 @@ object NativeLoader {
      * Loads library from minecraft Resource
      */
     fun load(lib: Resource) {
-        LOGGER.trace("Try load native from ${lib.location}")
+        LOGGER.debug("Try load native from ${lib.location}")
         val tempFile = Files.createTempFile("IngameIME-Native", null).apply {
-            LOGGER.trace("Copying Native to $this")
+            LOGGER.debug("Copying Native to $this")
             Files.copy(lib.inputStream, this, StandardCopyOption.REPLACE_EXISTING)
         }
         System.load(tempFile.toAbsolutePath().toString())
