2426c9a856ee6d5452997d296ac882b87cddfbe0
Windmill-City <1449182174@qq.com>
2021-04-18 14:50:17 +0800

    skip upload if already uploaded

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -43,7 +43,7 @@ base {
 dependencies {
     minecraft("com.mojang:minecraft:${minecraft_version}")
     mappings(minecraft.officialMojangMappings())
-    
+
     //Fabric
     modImplementation("net.fabricmc:fabric-loader:${fabric_loader_version}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${fabric_api_version}")
@@ -105,7 +105,7 @@ curseforge {
             requiredDependency("cloth-api")
         })
     })
-    
+
     options(closureOf<Options> {
         forgeGradleIntegration = false
     })
