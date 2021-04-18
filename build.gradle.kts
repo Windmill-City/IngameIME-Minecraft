953cff216f4cb859db43e45d888f200d1d29cab1
Windmill-City <1449182174@qq.com>
2021-04-18 14:48:49 +0800

    skip upload if already uploaded

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,89 @@ subprojects {
         withType<KotlinCompile> {
             kotlinOptions.jvmTarget = "1.8"
         }
-    
+
         withType<JavaCompile> {
             sourceCompatibility = "1.8"
             targetCompatibility = "1.8"
+        }
+    }
+    class Version(version: String) : Comparable<Version> {
+        @Suppress("PropertyName")
+        val VERSION_REGEX: Regex = Regex("""(\d+)(?:.(\d+)(?:.(\d+))?)?""")
+        var major: Int = 0
+        var minor: Int = 0
+        var revision: Int = 0
+
+        init {
+            VERSION_REGEX.matchEntire(version)?.apply {
+                major = this.groupValues[1].toInt()
+                minor = this.groupValues[2].toIntOrNull() ?: 0
+                revision = this.groupValues[3].toIntOrNull() ?: 0
+            } ?: throw IllegalArgumentException("Invalid version string:$version")
+        }
+
+        override fun compareTo(other: Version): Int {
+            if (this == other) return 0
+            if (this.major > other.major) return 1
+            if (this.major == other.major) {
+                if (this.minor > other.minor) return 1
+                if (this.minor == other.minor && this.revision > other.revision) return 1
+            }
+            return -1
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Version
+
+            if (major != other.major) return false
+            if (minor != other.minor) return false
+            if (revision != other.revision) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = major
+            result = 31 * result + minor
+            result = 31 * result + revision
+            return result
+        }
+
+        override fun toString(): String {
+            return "$major.$minor.$revision"
+        }
+    }
+
+    afterEvaluate {
+        tasks {
+            withType<com.matthewprenger.cursegradle.CurseUploadTask> {
+                val curseforgeFile = file("../CurseForgeLatest.json")
+                val versionInfo =
+                    (groovy.json.JsonSlurper().parse(curseforgeFile) as Map<String, String>).toMutableMap()
+                onlyIf {
+                    val uploadedVersion = Version(versionInfo[project.name]!!)
+                    val currentVersion = Version(project.version.toString())
+                    println("Uploaded:$uploadedVersion")
+                    println("Current:$currentVersion")
+                    return@onlyIf uploadedVersion < currentVersion
+                }
+                doLast {
+                    //Uploaded, update json file
+                    versionInfo[project.name] = project.version.toString()
+                    groovy.json.JsonOutput.toJson(versionInfo).let {
+                        curseforgeFile
+                            .outputStream()
+                            .bufferedWriter().apply {
+                                write(groovy.json.JsonOutput.prettyPrint(it))
+                                flush()
+                                close()
+                            }
+                    }
+                }
+            }
         }
     }
 }
