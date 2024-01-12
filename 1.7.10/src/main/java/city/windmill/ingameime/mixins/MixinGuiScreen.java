45857318ac66536f48e39fa06ad88c621def516a
Windmill_City <1449182174@qq.com>
2024-01-12 17:58:42 +0800

    cleanup imports

--- 1.7.10/src/main/java/city/windmill/ingameime/mixins/MixinGuiScreen.java
+++ 1.7.10/src/main/java/city/windmill/ingameime/mixins/MixinGuiScreen.java
@@ -3,7 +3,6 @@ package city.windmill.ingameime.mixins;
 import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(GuiScreen.class)
 public interface MixinGuiScreen {
